f0b87de50bd71b0e25236632a825bdc4b86b66bd
Chung Nguyen <zrmedia9@gmail.com>
2023-08-15 09:29:31 +0700

    Update code to lesson 73

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Banner from '../components/banner'
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import Banner from "../components/banner";
+import Card from "../components/card";
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
-
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
-  }
-
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -18,11 +19,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                key={coffeeStore.id}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+                className={styles.card}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
-  )
+  );
 }
